70aa4bbc3f975dcdd7a0ba0d87b75c583ed0ee04
mijaelino21-debug <mijaelino21@gmail.com>
2026-09-10 14:16:47 -0500

    feat: crea modelos Question y Choice con sus relaciones, y migracion

--- src/quiz/models.py
+++ src/quiz/models.py
@@ -7,3 +7,18 @@ class Exam(models.Model):
 
     def __str__(self):
         return self.title
+
+class Question(models.Model):
+    statement = models.TextField()
+    exam = models.ForeignKey(Exam, related_name="questions", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.statement
+
+class Choice(models.Model):
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, related_name="choices", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
