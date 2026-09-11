ddbfeb770e8c7b912cc4cdd90b352ea7a1a893c5
mijaelino21-debug <mijaelino21@gmail.com>
2026-09-10 19:42:42 -0500

    feat: registra modelos Exam, Question y Choice en el admin con inline de Choice

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Exam, Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('statement', 'exam')
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct')
